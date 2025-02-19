64decff8b387b1c08a37bbe01c627badf495cf42
Namia-X <mk258855@outlook.com>
2025-02-20 01:27:17 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -3,7 +3,10 @@
 # DIY扩展二合一了，在此处可以增加插件
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
-src-git modem https://github.com/FUjr/modem_feeds.git;main
+echo >> feeds.conf.default
+echo 'src-git modem https://github.com/FUjr/modem_feeds.git;main' >> feeds.conf.default
+./scripts/feeds update modem
+./scripts/feeds install -a -p modem
 # 1----------------------------------------------------------------------------------------------------------------------------------
 
 # 后台IP设置
